2a023a67578c8422d3f5d6c8d0d5f5c2ec0dcfe6
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-02-22 22:10:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let numbers = [2, 3, 5, 7, 9, 92, 928, 228, 38]
+
+console.log(numbers[3])
+
+// set numbers
+
+numbers[2] = 34;
+console.log(numbers)
---
+++ length.js
@@ -0,0 +1,3 @@
+let numbers = [2, 3, 5, 7, 9, 92, 928, 228, 38]
+
+console.log(numbers.length)
