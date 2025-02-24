839c425e672b06e9538a587893052439b6f1f5e7
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-02-24 14:36:32 +0600

    learned push pop

---
+++ pushpop.js
@@ -0,0 +1,10 @@
+const numbers = [56 , 59, 84, 90]
+
+console.log(numbers)
+
+numbers.push(45,98, 34, 23)
+
+console.log(numbers)
+
+numbers.pop()
+console.log(numbers)
---
+++ shift.js
@@ -0,0 +1,5 @@
+const numbers = [56 , 59, 84, 90]
+
+numbers.unshift(34)
+
+console.log(numbers)
